95ebac10d9d4089f01e121ecf30523d426cb5504
MZC01-ALTAIR <altair@megazone.com>
2023-05-22 12:05:25 +0900

    3

--- gateway/src/main/java/altairlab/com/gateway/GatewayApplication.java
+++ gateway/src/main/java/altairlab/com/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
