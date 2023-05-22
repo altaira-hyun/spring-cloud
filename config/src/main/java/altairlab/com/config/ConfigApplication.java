6d46c1866273bfe7982f81e892d3eb634025cdeb
MZC01-ALTAIR <altair@megazone.com>
2023-05-22 12:08:23 +0900

    4

--- config/src/main/java/altairlab/com/config/ConfigApplication.java
+++ config/src/main/java/altairlab/com/config/ConfigApplication.java
@@ -2,10 +2,11 @@ package altairlab.com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
