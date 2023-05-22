325fb64e034481ffa1b78af4bfb4b6b3e0659c3c
MZC01-ALTAIR <altair@megazone.com>
2023-05-22 12:11:35 +0900

    6

--- config/src/main/java/altairlab/com/config/ConfigApplication.java
+++ config/src/main/java/altairlab/com/config/ConfigApplication.java
@@ -3,6 +3,7 @@ package altairlab.com.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigServer
