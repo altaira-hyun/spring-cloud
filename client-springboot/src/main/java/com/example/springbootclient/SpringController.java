cf047a9562b3a78885308c997370afb8caeb427a
MZC01-ALTAIR <altair@megazone.com>
2023-05-23 14:46:02 +0900

    20

--- client-springboot/src/main/java/com/example/springbootclient/SpringController.java
+++ client-springboot/src/main/java/com/example/springbootclient/SpringController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RefreshScope
 @RestController
-public class SpringController {
+public class SimpleController {
 
-    @GetMapping("/spring-service")
-    public String springController() {
-        return "spring controller response";
+    @Value("${example.name}")
+    private String name;
+
+    @Value("${example.type}")
+    private String type;
+
+    @GetMapping("")
+    public String simple(){
+        return "name: " + name + " / type: " + type;
     }
 }
