0b8fbc1f985f3e93f3115241e54a8f08afb56afb
MZC01-ALTAIR <altair@megazone.com>
2023-05-23 15:54:29 +0900

    21

--- client-springboot/src/main/java/com/example/springbootclient/SpringController.java
+++ client-springboot/src/main/java/com/example/springbootclient/SpringController.java
@@ -2,6 +2,7 @@ package com.example.springbootclient;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @RefreshScope
