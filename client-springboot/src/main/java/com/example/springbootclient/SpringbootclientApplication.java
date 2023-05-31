ec3a76df95f29a365ff664709accf90d8a9f6b17
MZC01-ALTAIR <altair@megazone.com>
2023-05-31 15:49:17 +0900

    123

--- client-springboot/src/main/java/com/example/springbootclient/SpringbootclientApplication.java
+++ client-springboot/src/main/java/com/example/springbootclient/SpringbootclientApplication.java
@@ -3,8 +3,11 @@ package com.example.springbootclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
 public class SpringbootclientApplication {
 
